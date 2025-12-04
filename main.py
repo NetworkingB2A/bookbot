1dfd46ec371bb1f8aee1367be765f55be5634d2f
Adam Angell <adam.angell@networkingb2a.com>
2025-12-03 23:42:00 -0600

    Base project completed.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from stats import get_number_of_words, get_char_dict, get_sorted_dict_list
+import sys
+from stats import (
+   get_number_of_words, 
+   get_char_dict, 
+   get_sorted_dict_list
+   )
 
 def get_book_text(file_path: str) -> str:
     with open(file_path, mode="r", encoding='utf-8-sig') as file:
@@ -12,14 +17,18 @@ def print_report(list_char):
     return list_text.rstrip()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+      print("Usage: python3 main.py <path_to_book>")
+      sys.exit(1) 
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     num_words = get_number_of_words(book_text)
     letter_count = get_char_dict(book_text)
     sorted_letter_count = get_sorted_dict_list(letter_count)
     
 
-
+#     #TODO: Move this var up to a function
+#     #EXTRA: I could convert this over to a jinja2 template not needed but would be cooler
     report_var = f"""
 ============ BOOKBOT ============
 Analyzing book found at { book_path}...
